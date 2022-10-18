63e92daf71fceaccbf978eb74d0bebec4ecc92db
montgomeri15 <montgomeri15@gmail.com>
2022-10-18 15:42:33 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {0, 2, 3, -9, 4, -29, 50, -34, -32};
+        getEvenNumbers(numbers);
+    }
+
+    private static void getEvenNumbers(int[] numbers) {
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.println(num + "\t- парне число");
+            }
+        }
     }
 }
